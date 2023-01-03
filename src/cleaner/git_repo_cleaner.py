252681ecae2a073241da470378d7a7fe2efad90c
Vishnu-Naik <vishnugnaik@gmail.com>
2023-01-03 23:35:36 +0100

    Decoupling fetching repo object

--- src/cleaner/git_repo_cleaner.py
+++ src/cleaner/git_repo_cleaner.py
@@ -12,14 +12,14 @@ class GitRepoCleaner:
     """
 
     @staticmethod
-    def get_local_and_remote_branches(repo_path: str = None) -> tuple[list[str], list[str]]:
-        """Get branches from local and remote repository
+    def get_repo(repo_path: str) -> Repo:
+        """Get git repository object
 
         Args:
             repo_path (str): Path to local git repository
 
         Returns:
-            tuple: Tuple of lists of local and remote branches
+            Repo: Git repository object
 
         Raises:
             AssertionError: If repository is bare
@@ -37,6 +37,20 @@ class GitRepoCleaner:
 
         assert not repo.bare
 
+        return repo
+
+    @staticmethod
+    def get_local_and_remote_branches(repo_path: str = None) -> tuple[list[str], list[str]]:
+        """Get branches from local and remote repository
+
+        Args:
+            repo_path (str): Path to local git repository
+
+        Returns:
+            tuple: Tuple of lists of local and remote branches
+        """
+        repo = GitRepoCleaner.get_repo(repo_path)
+
         remote_refs = repo.remote().refs
         remote_branches = []
         local_branches = []
@@ -50,17 +64,17 @@ class GitRepoCleaner:
         return local_branches, remote_branches
 
     @staticmethod
-    def delete_branches(local_branches: list[str], remote_branches: list[str]) -> None:
+    def delete_branches(repo_path: str) -> None:
         """Delete branches which are not in remote repository
 
         Args:
-            local_branches (list[str]): List of local branches
-            remote_branches (list[str]): List of remote branches
+            repo_path(str): Path to local git repository
 
         Returns:
             None
         """
-
+        repo = GitRepoCleaner.get_repo(repo_path)
+        local_branches, remote_branches = GitRepoCleaner.get_local_and_remote_branches(repo_path)
         local_branches_not_on_remote = list(set(local_branches).symmetric_difference(set(remote_branches)))
         is_check = input(f"There {'is' if len(local_branches_not_on_remote) == 1 else 'are'} "
                          f"{len(local_branches_not_on_remote)} branch[s] which are not in remote repository. "
@@ -74,7 +88,7 @@ class GitRepoCleaner:
 
                     if permission_given.upper == "Y":
                         print(f"Deleting branch: {branch} ...")
-                        # repo.delete_head(branch, force=True)
+                        repo.delete_head(branch, force=True)
                         print(f"{branch} is deleted from local repository")
                     else:
                         print(f"Branch {branch} is not deleted")
@@ -83,6 +97,7 @@ class GitRepoCleaner:
         else:
             print('[INFO] You can delete them manually by running: git branch -d <branch_name> (for local) '
                   'or git push origin --delete <branch_name> (for remote)')
+
 
 
 def main(repo_path: str = '') -> None:
@@ -103,7 +118,7 @@ def main(repo_path: str = '') -> None:
     if len(remote_branches) == len(local_branches):
         print("ALL OK!!! \nYour local repository and remote are in sync\n")
     elif len(remote_branches) < len(local_branches):
-        GitRepoCleaner.delete_branches(local_branches, remote_branches)
+        GitRepoCleaner.delete_branches(repo_path)
     else:
         print("Looks like you have some branches in remote repository which are not in local repository. "
               "Try to fetch them first by running: git fetch")
